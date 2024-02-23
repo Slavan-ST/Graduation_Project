a7b3cb7242a298ce9704004a36367a30cf7362b3
Slavan-ST <slavan990900000@gmail.com>
2024-02-24 01:28:07 +0700

    добавил дто для пользователя

---
+++ Основное приложение/ClientServer/Helper/DTO/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Helper.DTO
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; } = "";
+        public string? Surname { get; set; } = "";
+        public string? Patronymic { get; set; } = "";
+        public string Login { get; set; } = "";
+        public byte[]? Image { get; set; }
+        public int RoleId { get; set; }
+    }
+}
