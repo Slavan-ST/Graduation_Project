95bb51ecfb2f7103621c4e3387884c0964ac243a
Slavan-ST <slavan990900000@gmail.com>
2024-03-06 00:05:51 +0700

    добавил классы, через которые буду обращаться к серверу

---
+++ Основное приложение/ClientServer/Client/Client/Data/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Data
+{
+    internal static class Room
+    {
+        //тут будет получение и отправка на сервер
+    }
+}
