2f31d641f704ad524ea36e328dad600a12dc7fb7
Slavan-ST <slavan990900000@gmail.com>
2024-03-06 01:21:24 +0700

    начал делать заготовки для работы с API на клиенте

--- Основное приложение/ClientServer/Client/Client/Data/AttendanceLog.cs
+++ Основное приложение/ClientServer/Client/Client/Data/AttendanceLog.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Helper;
+using Helper.Models.DTO;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +13,26 @@ namespace Client.Data
 {
     internal static class AttendanceLog
     {
-        //тут будет получение и отправка на сервер
+        //тут будет получение и отправка на сервер get, post,put,delete
+        //AttendanceLog
+
+        //Test
+        //вариант 1 сделать каждый класс по отдельности
+        //вариант 2 сделать универсальный через параметры(мб не очень надёжно)
+
+        public static async Task<AttendanceLogDTO?> GetAttendanceLog(params (string name, string value)[] pars)//id и прочее
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                var response = await client.GetFromJsonAsync<AttendanceLogDTO>(Connect.Connection + $"AttendanceLog");
+                return response;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
     }
 }
