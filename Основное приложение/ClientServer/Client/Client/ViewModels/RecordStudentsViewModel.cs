c061b966d439bb1da2551b153653fb4023469233
Slavan-ST <slavan990900000@gmail.com>
2024-03-24 16:26:04 +0700

    save

--- Основное приложение/ClientServer/Client/Client/ViewModels/RecordStudentsViewModel.cs
+++ Основное приложение/ClientServer/Client/Client/ViewModels/RecordStudentsViewModel.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
+using System.Data.Common;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -18,8 +19,7 @@ namespace Client.ViewModels
     public class RecordStudentsViewModel : ViewModelBase
     {
         [Reactive]
-        public DataView People { get; set; }
-        public DataTable PeopleTable { get; set; }
+        public DataTable People { get; set; } = null!;
 
         public RecordStudentsViewModel(IScreen? screen = null) : base(screen)
         {
@@ -28,8 +28,7 @@ namespace Client.ViewModels
 
         async void TestPeoples()
         {
-            PeopleTable = await MakeTable();
-            People = PeopleTable.DefaultView;
+            People = await MeVariant(2023, 11);
         }
 
         async Task<DataTable> MakeTable()
@@ -38,7 +37,8 @@ namespace Client.ViewModels
             DataTable dataTable = new DataTable();
             dataTable.Columns.Add("FIO");
 
-            foreach (var item in logDTOs)
+            var prevDate = DateTime.MinValue;
+            foreach (var item in logDTOs!)
             {
                 if (!dataTable.Columns.Contains(item.Date.Day.ToString()))
                 {
@@ -59,13 +59,40 @@ namespace Client.ViewModels
                     row[day] = item.Marker?.Char;
                     prevFullName = fullName;
                 }
-                else
+                day = item.Date.Day.ToString();
+                row![day] = item.Marker?.Char;
+            }
+
+            return dataTable;
+        }
+        async Task<DataTable> MeVariant(int year, int month)
+        {
+            IEnumerable<AttendanceLogDTO>? logDTOs = await API.AttendanceLog.GetAttendanceLogs();
+            DataTable dataTable = new DataTable();
+
+
+            dataTable.Columns.Add("FIO");
+            for (int i = 0; i < month; i++)
+            {
+                dataTable.Columns.Add(new DataColumn((i + 1).ToString()));
+            }
+
+            if (logDTOs != null)
+            {
+                var logs = logDTOs.Where(x => x.Date.Year == year && x.Date.Month == month).OrderBy(x => x.Date).ToList(); //логи текущего месяца, отсортированные 
+
+                DataRow? row = dataTable.NewRow();
+
+                for (int i = 1; i < dataTable.Columns.Count; i++)
                 {
-                    day = item.Date.Day.ToString();
-                    row[day] = item.Marker?.Char;
+                    row[dataTable.Columns[i].ColumnName] = logs.Where(x => x.Student!.Surname + " " + x.Student!.Name + " " + x.Student!.Patronymic == row["FIO"].ToString() && x.Date.Day.ToString() == dataTable.Columns[i].ColumnName).FirstOrDefault();
                 }
                 dataTable.Rows.Add(row);
+
             }
+
+            
+
 
             return dataTable;
         }
