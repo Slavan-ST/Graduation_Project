94d1dbd575c8b4b1b0673dc0975597ef96a48f7d
SteaMMe123@ya.ru <SteaMMe123@ya.ru>
2024-03-24 18:58:15 +0700

    edit rules datagrid

--- Основное приложение/ClientServer/Client/Client/Views/RecordStudentsView.axaml.cs
+++ Основное приложение/ClientServer/Client/Client/Views/RecordStudentsView.axaml.cs
@@ -12,26 +12,6 @@ namespace Client.Views
         {
             this.WhenActivated(disposables => { });
             AvaloniaXamlLoader.Load(this);
-            if (DataContext is RecordStudentsViewModel)
-            {
-                //clear out any existing columns
-                while (ExampleDatagrid.Columns.Count > 0) { ExampleDatagrid.Columns.RemoveAt(ExampleDatagrid.Columns.Count - 1); }
-                //assign the datatable to the grid
-                ExampleDatagrid.ItemsSource = (DataContext as RecordStudentsViewModel).PeopleTable.DefaultView;
-
-                // create the grid columns based on the datatables columns
-                foreach (System.Data.DataColumn x in (DataContext as RecordStudentsViewModel).PeopleTable.Columns)
-                {
-                    if (x.DataType == typeof(bool))
-                    {
-                        ExampleDatagrid.Columns.Add(new DataGridCheckBoxColumn { Header = x.ColumnName, Binding = new Avalonia.Data.Binding($"Row.ItemArray[{x.Ordinal}]") });
-                    }
-                    else
-                    {
-                        ExampleDatagrid.Columns.Add(new DataGridTextColumn { Header = x.ColumnName, Binding = new Avalonia.Data.Binding($"Row.ItemArray[{x.Ordinal}]") });
-                    }
-                }
-            }
         }
     }
 }
