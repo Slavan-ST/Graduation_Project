d2ea2e80a253069a6f68c017c9b2f61c2db1a756
Slavan-ST <slavan990900000@gmail.com>
2024-04-09 08:24:03 +0700

    добавил контроллер для групп

---
+++ Основное приложение/ClientServer/WebAPI/Controllers/GroupController.cs
@@ -0,0 +1,105 @@
+﻿using Helper.Models.Main;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GroupController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Group>>> GetGroups()
+        {
+            ApplicationContext db = new ApplicationContext();
+            var groups = await db.Groups.ToListAsync();
+            if (groups == null)
+            {
+                return NotFound();
+            }
+            await db.DisposeAsync();
+
+            return new JsonResult(groups);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> PostGroup(Group? groupDTO)
+        {
+            if (groupDTO == null)
+            {
+                return NoContent();
+            }
+            ApplicationContext db = new ApplicationContext();
+
+            //проверка на существование такой записи в БД
+            Group? group = await db.Groups
+                .Where(x => x.Id == groupDTO.Id)
+                .FirstOrDefaultAsync();
+
+            if (group != null)
+            {
+                return StatusCode(400);
+            }
+
+            group = groupDTO;
+
+            await db.Groups.AddAsync(group!);
+            await db.SaveChangesAsync();
+            await db.DisposeAsync();
+
+            return StatusCode(201);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutGroup(Group? groupDTO)
+        {
+            if (groupDTO == null)
+            {
+                return NoContent();
+            }
+
+            ApplicationContext db = new ApplicationContext();
+            //проверка на существование такой записи в БД
+            Group? group = await db.Groups
+                .Where(x => x.Id == groupDTO.Id)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+
+            if (group == null)
+            {
+                return StatusCode(404);
+            }
+
+            group = groupDTO;
+
+            db.Groups.Update(group!);
+            await db.SaveChangesAsync();
+            await db.DisposeAsync();
+
+            return StatusCode(202);//принято
+        }
+
+        [HttpDelete("{name}")]
+        public async Task<ActionResult> DeleteGroup(string name)
+        {
+            ApplicationContext db = new ApplicationContext();
+
+            var group = await db.Groups
+                .Where(x => x.Name == name)
+                .FirstOrDefaultAsync();
+
+            if (group == null)
+            {
+                return StatusCode(404);
+            }
+
+            db.Groups.Remove(group);
+            await db.SaveChangesAsync();
+            await db.DisposeAsync();
+
+            return StatusCode(202);//принято
+        }
+    }
+}
