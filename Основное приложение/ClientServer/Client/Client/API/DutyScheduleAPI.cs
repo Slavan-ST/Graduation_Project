5e5b68b9f614386ff1fcf51f333147941e9b85c3
Slavan-ST <slavan990900000@gmail.com>
2024-04-09 10:49:51 +0700

    добавил связь с апи на стороне клиента для новых графиков

--- Основное приложение/ClientServer/Client/Client/API/DutyScheduleAPI.cs
+++ Основное приложение/ClientServer/Client/Client/API/DutyScheduleAPI.cs
@@ -1,13 +1,138 @@
-﻿using System;
+﻿using Helper;
+using Helper.Models.Main;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Client.API
 {
-    internal class DutyScheduleAPI
+    internal static class DutyScheduleAPI
     {
+        public static async Task<DutySchedule?> GetDutySchedule(int id)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                var response = await client.GetFromJsonAsync<DutySchedule>(Connect.Connection + $"DutySchedule/{id}");
+                return response;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
 
+        public static async Task<IEnumerable<DutySchedule>?> GetDutySchedules()
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                var response = await client.GetFromJsonAsync<IEnumerable<DutySchedule>>(Connect.Connection + $"DutySchedule");
+                return response;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
+
+        public static async Task<IEnumerable<DutySchedule>?> GetDutySchedulesYear(int year)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                var response = await client.GetFromJsonAsync<IEnumerable<DutySchedule>>(Connect.Connection + $"DutySchedule/year:{year}");
+                return response;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
+
+        public static async Task<IEnumerable<DutySchedule>?> GetDutySchedulesMonth(int year, int month)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                var response = await client.GetFromJsonAsync<IEnumerable<DutySchedule>>(Connect.Connection + $"DutySchedule/month:{month}.{year}");
+                return response;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
+
+        public static async Task<IEnumerable<DutySchedule>?> GetDutySchedulesDay(int year, int month, int day)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                var response = await client.GetFromJsonAsync<IEnumerable<DutySchedule>>(Connect.Connection + $"DutySchedule/day:{day}.{month}.{year}");
+                return response;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
+
+
+        public static async Task<HttpStatusCode?> DeleteDutySchedule(int id)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                var response = await client.DeleteAsync(Connect.Connection + $"DutySchedule/{id}");
+                return response.StatusCode;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
+        //создание лога
+        public static async Task<HttpStatusCode?> PostDutySchedule(DutySchedule dutySchedule)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                var response = await client.PostAsJsonAsync(Connect.Connection + $"DutySchedule", dutySchedule);
+                return response.StatusCode;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
+        //обновление лога
+        public static async Task<HttpStatusCode?> PutDutySchedule(DutySchedule dutySchedule)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                var response = await client.PutAsJsonAsync(Connect.Connection + $"DutySchedule", dutySchedule);
+                return response.StatusCode;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
     }
 }
--- Основное приложение/ClientServer/Client/Client/API/PurityRaidLogAPI.cs
+++ Основное приложение/ClientServer/Client/Client/API/PurityRaidLogAPI.cs
@@ -1,12 +1,138 @@
-﻿using System;
+﻿using Helper;
+using Helper.Models.Main;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Client.API
 {
-    internal class PurityRaidLogAPI
+    internal static class PurityRaidLogAPI
     {
+        public static async Task<PurityRaidLog?> GetPurityRaidLog(int id)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                var response = await client.GetFromJsonAsync<PurityRaidLog>(Connect.Connection + $"PurityRaidLog/{id}");
+                return response;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
+
+        public static async Task<IEnumerable<PurityRaidLog>?> GetPurityRaidLogs()
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                var response = await client.GetFromJsonAsync<IEnumerable<PurityRaidLog>>(Connect.Connection + $"PurityRaidLog");
+                return response;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
+
+        public static async Task<IEnumerable<PurityRaidLog>?> GetPurityRaidLogsYear(int year)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                var response = await client.GetFromJsonAsync<IEnumerable<PurityRaidLog>>(Connect.Connection + $"PurityRaidLog/year:{year}");
+                return response;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
+
+        public static async Task<IEnumerable<PurityRaidLog>?> GetPurityRaidLogsMonth(int year, int month)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                var response = await client.GetFromJsonAsync<IEnumerable<PurityRaidLog>>(Connect.Connection + $"PurityRaidLog/month:{month}.{year}");
+                return response;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
+
+        public static async Task<IEnumerable<PurityRaidLog>?> GetPurityRaidLogsDay(int year, int month, int day)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                var response = await client.GetFromJsonAsync<IEnumerable<PurityRaidLog>>(Connect.Connection + $"PurityRaidLog/day:{day}.{month}.{year}");
+                return response;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
+
+
+        public static async Task<HttpStatusCode?> DeletePurityRaidLog(int id)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                var response = await client.DeleteAsync(Connect.Connection + $"PurityRaidLog/{id}");
+                return response.StatusCode;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
+        //создание лога
+        public static async Task<HttpStatusCode?> PostPurityRaidLog(PurityRaidLog purityRaidLog)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                var response = await client.PostAsJsonAsync(Connect.Connection + $"PurityRaidLog", purityRaidLog);
+                return response.StatusCode;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
+        //обновление лога
+        public static async Task<HttpStatusCode?> PutPurityRaidLog(PurityRaidLog purityRaidLog)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                var response = await client.PutAsJsonAsync(Connect.Connection + $"PurityRaidLog", purityRaidLog);
+                return response.StatusCode;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
     }
 }
