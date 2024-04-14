edb132eb0b39a694c6a2a4c9f4b30a13b8f2c50c
Slavan-ST <slavan990900000@gmail.com>
2024-04-14 19:45:53 +0700

    fix property

--- Основное приложение/ClientServer/Client/Client/Controls/EditableTextBlock.axaml.cs
+++ Основное приложение/ClientServer/Client/Client/Controls/EditableTextBlock.axaml.cs
@@ -1,24 +1,42 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
 using Avalonia.Controls.Primitives;
+using Avalonia.Markup.Xaml.Styling;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using ReactiveUI.Fody.Helpers;
+using System;
 
 namespace Client.Controls
 {
+    [TemplatePart("EditableTextBlockPresenter", typeof(ItemsControl))]
     public class EditableTextBlock : TemplatedControl
     {
-        public static readonly StyledProperty<string> InsideTextPropety = AvaloniaProperty.Register<EditableTextBlock, string>(nameof(InsideText));
+
+        static EditableTextBlock()
+        {
+            Application.Current?.Resources.MergedDictionaries
+                .Add(new ResourceInclude(new Uri("avares://Client/Controls/EditableTextBlock.axaml"))
+                {
+                    Source = new Uri("avares://Client/Controls/EditableTextBlock.axaml")
+                });
+        }
+        public EditableTextBlock()
+        {
+
+        }
+
+        public static readonly StyledProperty<string> InsideTextProperty = AvaloniaProperty.Register<EditableTextBlock, string>(nameof(InsideText));
 
         public string InsideText
         {
             get
             {
-                return GetValue(InsideTextPropety);
+                return GetValue(InsideTextProperty);
             }
             set
             {
-                SetValue(InsideTextPropety, value);
+                SetValue(InsideTextProperty, value);
             }
         }
     }
