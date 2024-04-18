c380cee6bf1cb9708a57670541b5ccfe93403f6d
Slavan-ST <slavan990900000@gmail.com>
2024-04-19 00:27:32 +0700

    end test pdf

---
+++ Основное приложение/ClientServer/Client/Client/Services/StatementCreater.cs
@@ -0,0 +1,49 @@
+﻿
+
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Client.Services
+{
+    public class StatementCreater
+    {
+        /// <summary>
+        /// Создание заполненого заявления в PDF
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="surname"></param>
+        /// <param name="patronymic"></param>
+        /// <param name="phone"></param>
+        /// <param name="room"></param>
+        /// <param name="dateOut"></param>
+        /// <param name="dateIn"></param>
+        public static void CreateStatement(string name, string surname, string patronymic, string phone, string room, string dateOut, string dateIn)
+        {
+            using var document = new Document(new PdfDocument(new PdfWriter("helloworld-pdf.pdf")));
+
+            var par1 = new Paragraph("right text").SetTextAlignment(iText.Layout.Properties.TextAlignment.RIGHT);
+
+            var par2 = new Paragraph("center text").SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER);
+
+            var par3 = new Paragraph("left text").SetTextAlignment(iText.Layout.Properties.TextAlignment.LEFT);
+
+
+
+            document.Add(par1);
+            document.Add(par2);
+            document.Add(par3);
+        }
+        /// <summary>
+        /// создание пустого заявления в PDF
+        /// </summary>
+        public static void CreateStatementEmpty()
+        {
+            using var document = new Document(new PdfDocument(new PdfWriter("helloworld-pdf.pdf")));
+            document.Add(new Paragraph("Hello World!"));
+            //RT.ViewPDF(new Test(), "Test.pdf");
+        }
+    }
+}
--- Основное приложение/ClientServer/Client/Client/ViewModels/StatementViewModel.cs
+++ Основное приложение/ClientServer/Client/Client/ViewModels/StatementViewModel.cs
@@ -1,18 +1,45 @@
-﻿using Client.ViewModels.Base;
+﻿using Client.Services;
+using Client.ViewModels.Base;
 using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Client.ViewModels
 {
     public class StatementViewModel : ViewModelBase
     {
-        public StatementViewModel(IScreen? screen = null) : base(screen) 
+        public StatementViewModel(IScreen? screen = null) : base(screen)
         {
-
+            GetEmptyStatement = ReactiveCommand.Create(() =>
+            {
+                StatementCreater.CreateStatement(Name, Surname, Patronymic, Phone, Room, DateOut, DateIn);
+            });
+            GetFillStatement = ReactiveCommand.Create(() =>
+            {
+                StatementCreater.CreateStatement(Name, Surname, Patronymic, Phone, Room, DateOut, DateIn);
+            });
         }
+        [Reactive]
+        public string Surname { get; set; } = string.Empty;
+        [Reactive]
+        public string Name { get; set; } = string.Empty;
+        [Reactive]
+        public string Patronymic { get; set; } = string.Empty;
+        [Reactive]
+        public string Room { get; set; } = string.Empty;
+        [Reactive]
+        public string Phone { get; set; } = string.Empty;
+        [Reactive]
+        public string DateOut { get; set; } = string.Empty;
+        [Reactive]
+        public string DateIn { get; set; } = string.Empty;
+
+        public ICommand GetEmptyStatement { get; set; }
+        public ICommand GetFillStatement { get; set; }
     }
 }
