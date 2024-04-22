4c960d4779247f22fd8ad711553e13f6485e5522
Slavan-ST <slavan990900000@gmail.com>
2024-04-22 11:51:13 +0700

    update xaml

--- Основное приложение/ClientServer/Client/Client/ViewModels/CleanLineRaidViewModel.cs
+++ Основное приложение/ClientServer/Client/Client/ViewModels/CleanLineRaidViewModel.cs
@@ -1,10 +1,18 @@
 ﻿using Client.ViewModels.Base;
+using Helper.Models.Main;
 using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System.Collections.Generic;
 
 namespace Client.ViewModels
 {
     public class CleanLineRaidViewModel : ViewModelBase
     {
+        [Reactive]
+        public Room? SelectedRoom { get; set; } // для comboBox
+        [Reactive]
+        public List<Room>? Rooms { get; set; } // для comboBox, загружается только при старте страницы
+
         public CleanLineRaidViewModel(IScreen? screen = null) : base(screen) 
         {
 
