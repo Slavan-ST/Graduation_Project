29316738ff4aa603d5a10b2190d392d58ca553b0
NikolayIgnatyev <SteaMMe123@ya.ru>
2024-05-07 11:58:30 +0700

    added switch month on charViews

--- Основное приложение/ClientServer/Client/Client/ViewModels/DutyChartViewModel.cs
+++ Основное приложение/ClientServer/Client/Client/ViewModels/DutyChartViewModel.cs
@@ -5,25 +5,61 @@ using ReactiveUI.Fody.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Client.ViewModels
 {
     public class DutyChartViewModel : ViewModelBase
     {
+
+        [Reactive]
+        public string MonthString { get; set; } = string.Empty;
+        [Reactive]
+        public int MonthInt { get; set; } = DateTime.Now.Month;
+
         public DutyChartViewModel(IScreen? screen = null) : base(screen)
         {
+            MonthString = CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(MonthInt);
+            MonthNext = ReactiveCommand.Create(() =>
+            {
+                if(MonthInt != 12)
+                {
+                    MonthInt += 1;
+                    MonthString = CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(MonthInt);
+                    GetAsync();
+                }
+            });
+            MonthPrev = ReactiveCommand.Create(() =>
+            {
+                if(MonthInt != 1)
+                {
+                    MonthInt -= 1;
+                    MonthString = CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(MonthInt);
+                    GetAsync();
+                }
+            });
             GetAsync();
         }
 
         private async void GetAsync()
         {
-            DutyItems = await API.DutyScheduleAPI.GetDutySchedulesMonth(2023,6);
+            DutyItems = await API.DutyScheduleAPI.GetDutySchedulesMonth(DateTime.Now.Year,MonthInt);
         }
 
         [Reactive]
         public IEnumerable<DutySchedule>? DutyItems { get; set; }
+
+        /// <summary>
+        /// Переключается месяц вперед
+        /// </summary>
+        public ICommand MonthNext { get; set; }
+        /// <summary>
+        /// Переключается месяц назад
+        /// </summary>
+        public ICommand MonthPrev { get; set; }
     }
 }
--- Основное приложение/ClientServer/Client/Client/ViewModels/PurityChartViewModel.cs
+++ Основное приложение/ClientServer/Client/Client/ViewModels/PurityChartViewModel.cs
@@ -1,18 +1,61 @@
 ﻿using Client.ViewModels.Base;
+using Helper.Models.Main;
 using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Client.ViewModels
 {
     public class PurityChartViewModel : ViewModelBase
     {
+
+        [Reactive]
+        public string MonthString { get; set; } = string.Empty;
+        [Reactive]
+        public int MonthInt { get; set; } = DateTime.Now.Month;
+
         public PurityChartViewModel(IScreen? screen = null) : base(screen)
         {
-        
+            MonthString = CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(MonthInt);
+            MonthNext = ReactiveCommand.Create(() =>
+            {
+                if (MonthInt != 12)
+                {
+                    MonthInt += 1;
+                    MonthString = CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(MonthInt);
+                    GetAsync();
+                }
+            });
+            MonthPrev = ReactiveCommand.Create(() =>
+            {
+                if (MonthInt != 1)
+                {
+                    MonthInt -= 1;
+                    MonthString = CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(MonthInt);
+                    GetAsync();
+                }
+            });
+            GetAsync();
         }
+
+        private async void GetAsync()
+        {
+            // реализовать получение данных
+        }
+
+        /// <summary>
+        /// Переключается месяц вперед
+        /// </summary>
+        public ICommand MonthNext { get; set; }
+        /// <summary>
+        /// Переключается месяц назад
+        /// </summary>
+        public ICommand MonthPrev { get; set; }
     }
 }
