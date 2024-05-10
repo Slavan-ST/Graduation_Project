f7bb0d189757cbabeeafb0cbd8c63df7e3e69eed
Slavan-ST <slavan990900000@gmail.com>
2024-05-10 19:00:55 +0700

    добавил конфигурацию  подключения для клиента

---
+++ Основное приложение/ClientServer/Client/Client/Connect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using MsBox.Avalonia.Converters;
+using System.Globalization;
+
+namespace Client
+{
+    public static class Connect
+    {
+        public static string Connection
+        {
+            get
+            {
+                var config = new ConfigurationBuilder()
+                    .AddXmlFile("App.config")
+                    .Build();
+
+                string? connect = config["Connect"];
+
+                if (connect == null)
+                {
+                    return "http://localhost:8080/";
+                }
+                return connect;
+            }
+        }
+    }
+}
