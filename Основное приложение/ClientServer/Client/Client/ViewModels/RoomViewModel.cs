e288860e0453316af4e4717ab310bb8d635e688a
SteaMMe123@ya.ru <SteaMMe123@ya.ru>
2024-05-18 03:40:17 +0700

    Added RoomView and ViewModel

---
+++ Основное приложение/ClientServer/Client/Client/ViewModels/RoomViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Client.ViewModels.Base;
+using Helper.Models.Main;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Client.ViewModels
+{
+    public class RoomViewModel : ViewModelBase
+    {
+        [Reactive]
+        public List<Room> Rooms { get; set; }
+        [Reactive]
+        public Room SelectedRoom { get; set; }
+        
+        public ICommand NewRoom {  get; set; }
+        public ICommand Delete {  get; set; }
+        public ICommand Save { get; set; }
+        public RoomViewModel(IScreen? screen = null) : base(screen) 
+        {
+
+        }
+    }
+}
---
+++ Основное приложение/ClientServer/Client/Client/Views/RoomView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Client.Views
+{
+    public partial class RoomView : UserControl
+    {
+        public RoomView()
+        {
+            InitializeComponent();
+        }
+    }
+}
