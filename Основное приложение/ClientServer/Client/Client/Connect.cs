6492f42c0078f319b48deb475706c4992bfeba69
Slavan-ST <slavan990900000@gmail.com>
2024-05-31 12:36:32 +0700

    fix connect

--- Основное приложение/ClientServer/Client/Client/Connect.cs
+++ Основное приложение/ClientServer/Client/Client/Connect.cs
@@ -1,15 +1,4 @@
-﻿using Client.API;
-using Microsoft.Extensions.Configuration;
-using System;
-using System.Diagnostics;
-using System.Globalization;
-using System.Net.Http;
-using System.Net.Http.Json;
-using System.Net.NetworkInformation;
-using System.Threading;
-using System.Threading.Tasks;
-using static iText.StyledXmlParser.Jsoup.Select.Evaluator;
-
+﻿
 namespace Client
 {
     public static class Connect
