5a8fda663e6dee1080ea6992a609846ebbeb1d50
Slavan-ST <slavan990900000@gmail.com>
2024-06-05 10:27:08 +0700

    публик сите

--- main/ClientServer/Client/Client/Connect.cs
+++ main/ClientServer/Client/Client/Connect.cs
@@ -3,8 +3,8 @@ namespace Client
 {
     public static class Connect
     {
-        //static string _connection = "http://51.250.101.134:8888/";
-        static string _connection = "http://localhost:8080/";
+        static string _connection = "http://51.250.101.134:8888/";
+        //static string _connection = "http://localhost:8080/";
         public static string Connection
         {
             get
